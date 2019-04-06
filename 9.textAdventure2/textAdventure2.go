b6a0ab6877864e907a09db03a81be82dee4c0224
Eric Rhoads <ericr@kizan.com>
2019-04-06 13:14:19 -0400

    added a simple npc

--- 9.textAdventure2/textAdventure2.go
+++ 9.textAdventure2/textAdventure2.go
@@ -1,7 +1,6 @@
 package main
 
 // HOMEWORK
-// NPC - talk to them, fight
 // NPC - move around graph
 // items picked up or placed down
 // accept natural language as input
@@ -14,7 +13,13 @@ import (
 	"strings"
 )
 
-type choices struct {
+type npc struct {
+	name        string
+	description string
+	health      int
+}
+
+type choice struct {
 	cmd         string
 	description string
 	nextNode    *storyNode
@@ -22,18 +27,31 @@ type choices struct {
 
 type storyNode struct {
 	text    string
-	choices []*choices
+	choices []*choice
+	npc     []*npc
 }
 
 func (node *storyNode) addChoice(cmd string, description string, nextNode *storyNode) {
-	choice := &choices{cmd, description, nextNode}
+	choice := &choice{cmd, description, nextNode}
 	node.choices = append(node.choices, choice)
+}
+
+func (node *storyNode) addNPC(name string, description string, health int) {
+	character := &npc{name, description, health}
+	node.npc = append(node.npc, character)
 }
 
 func (node *storyNode) render() {
 	fmt.Println(node.text)
+	if node.npc != nil {
+		for _, character := range node.npc {
+			fmt.Println("\n You are engaged with a character.")
+			fmt.Println(" Name: ", character.name, "\n", "Description: ", character.description, "\n", "Health: ", character.health)
+		}
+	}
 	if node.choices != nil {
 		for _, choice := range node.choices {
+			fmt.Println()
 			fmt.Println(choice.cmd, choice.description)
 		}
 	}
@@ -79,11 +97,14 @@ func main() {
 	To the south, a passage appears to head upward. The eastern passage appears
 	flat and well traveled`}
 
-	darkRoom := storyNode{text: "It is pitch black. You cannot see a thing."}
-	darkRoomLit := storyNode{text: "The dark passage is now lit by your lantern. You can continue north or head back south."}
-	grue := storyNode{text: "While stumbling around in the darkness, you are eaten by a grue."}
-	trap := storyNode{text: "You head down the well traveled path when suddenly a trap door opens and you fall into a pit."}
-	treasure := storyNode{text: "You arrive at a small chamber, filled with treasure!"}
+	darkRoom := storyNode{text: "\n It is pitch black. You cannot see a thing."}
+	darkRoomLit := storyNode{text: "\n The dark passage is now lit by your lantern."}
+	grue := storyNode{text: "\n While stumbling around in the darkness, you are eaten by The Grue."}
+	trap := storyNode{text: "\n You head down the well traveled path when suddenly a trap door opens and you fall into a pit."}
+	treasure := storyNode{text: "\n You arrive at a small chamber, filled with treasure!"}
+	death := storyNode{text: "\n The Grue kills you."}
+	fight := storyNode{text: "\n You wound The Grue with your trusty sword. He lunges toward you, snarling."}
+	champion := storyNode{text: "\n You slay The Grue."}
 
 	start.addChoice("N", "Go North", &darkRoom)
 	start.addChoice("S", "Go South", &darkRoom)
@@ -92,9 +113,17 @@ func main() {
 	darkRoom.addChoice("S", "Try to go back south", &grue)
 	darkRoom.addChoice("O", "Turn on lantern", &darkRoomLit)
 
-	darkRoomLit.addChoice("N", "Go North", &treasure)
-	darkRoomLit.addChoice("S", "Go South", &start)
+	darkRoomLit.addNPC("The Grue", "A ferocious creature who feasts on human flesh.", 200)
+	darkRoomLit.addChoice("A", "Attack", &fight)
+	darkRoomLit.addChoice("R", "Run Away", &start)
 
+	fight.addNPC("The Grue", "A ferocious creature who feasts on human flesh.", 100)
+	fight.addChoice("R", "Run away", &death)
+	fight.addChoice("D", "Dodge the attack", &death)
+	fight.addChoice("A", "Attack again", &champion)
+
+	champion.addChoice("N", "Continue heading North", &treasure)
+	champion.addChoice("S", "Turn back South", &start)
 	start.play()
 
 	fmt.Println()
